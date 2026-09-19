b23a69678398c3eefac5fe26ba54c17678dadc97
Emmanuel Alozie <info@brickbybrique.ca>
2026-09-19 10:16:50 +0100

    Fix Prisma build failure on Vercel: externalize native packages, disable Turbopack for build

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     // Linting is handled separately in CI; don't block `next build` on it.
     ignoreDuringBuilds: true,
   },
+  serverExternalPackages: ["@prisma/client", "bcryptjs"],
 };
 
 module.exports = nextConfig;
